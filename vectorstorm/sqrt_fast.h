96b1b6b9abda206cf727a6c8f81628abc60d9eaf
Borealis <slowriot@voxelstorm.com>
2016-06-16 02:07:02 +0100

    Auto-aligning comments

--- vectorstorm/sqrt_fast.h
+++ vectorstorm/sqrt_fast.h
@@ -17,7 +17,7 @@ inline static float constexpr sqrt_inv_fast(float number) {
   float x = number * 0.5f;
   float y = number;
   uint32_t i  = *(uint32_t*)&y;                                                 // evil floating point bit level hacking
-  //i = 0x5f3759df - (i >> 1);                                                  // what the fuck?
+  //i = 0x5f3759df - (i >> 1);                                                    // what the fuck?
   i = 0x5f375a84 - (i >> 1);                                                    // improved magic number from http://jheriko-rtw.blogspot.co.uk/2009/04/understanding-and-improving-fast.html
   y = *(float*)&i;
   y = y * (threehalfs - (x * y * y));                                           // 1st iteration
@@ -32,7 +32,7 @@ inline static double constexpr sqrt_inv_fast(double number) {
   double x = number * 0.5;
   double y = number;
   uint64_t i  = *(uint64_t*)&y;                                                 // evil floating point bit level hacking
-  //i = 0x5fe6eb50c7b537a9ll - (i >> 1);                                        // even more magic than "what the fuck" number
+  //i = 0x5fe6eb50c7b537a9ll - (i >> 1);                                          // even more magic than "what the fuck" number
   uint64_t constexpr const magic = (uint64_t(0x5fe6eb50) << (8 * 4)) + uint64_t(0xc7b537a9); // hack to produce 0x5fe6eb50c7b537a9ll without triggering -Wlong-long warning
   i = magic - (i >> 1);
   y = *(double*)&i;
@@ -68,11 +68,11 @@ inline static float constexpr sqrt_inv_faster(float number) {
   float x = number * 0.5f;
   float y = number;
   uint32_t i  = *(uint32_t*)&y;                                                 // evil floating point bit level hacking
-  //i = 0x5f3759df - (i >> 1);                                                  // what the fuck?
+  //i = 0x5f3759df - (i >> 1);                                                    // what the fuck?
   i = 0x5f375a84 - (i >> 1);                                                    // improved magic number from http://jheriko-rtw.blogspot.co.uk/2009/04/understanding-and-improving-fast.html
   y = *(float*)&i;
   y = y * (threehalfs - (x * y * y));                                           // 1st iteration
-  //y = y * (threehalfs - (x * y * y));                                         // 2nd iteration, this can be removed
+  //y = y * (threehalfs - (x * y * y));                                           // 2nd iteration, this can be removed
   return y;
 }
 inline static double constexpr sqrt_inv_faster(double number) __attribute__((__always_inline__)) __attribute__((__optimize__("no-strict-aliasing")));
@@ -83,12 +83,12 @@ inline static double constexpr sqrt_inv_faster(double number) {
   double x = number * 0.5;
   double y = number;
   uint64_t i  = *(uint64_t*)&y;                                                 // evil floating point bit level hacking
-  //i = 0x5fe6eb50c7b537a9ll - (i >> 1);                                        // even more magic than "what the fuck" number
+  //i = 0x5fe6eb50c7b537a9ll - (i >> 1);                                          // even more magic than "what the fuck" number
   uint64_t constexpr const magic = (uint64_t(0x5fe6eb50) << (8 * 4)) + uint64_t(0xc7b537a9); // hack to produce 0x5fe6eb50c7b537a9ll without triggering -Wlong-long warning
   i = magic - (i >> 1);
   y = *(double*)&i;
   y = y * (threehalfs - (x * y * y));                                           // 1st iteration
-  //y = y * (threehalfs - (x * y * y));                                         // 2nd iteration, this can be removed
+  //y = y * (threehalfs - (x * y * y));                                           // 2nd iteration, this can be removed
   return y;
 }
 #pragma GCC diagnostic pop
