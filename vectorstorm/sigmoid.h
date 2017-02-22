2fde98505a9b62e64c364d7985a93c8acfd4d44a
SlowRiot <slowriot@voxelstorm.com>
2017-02-22 19:54:28 +0000

    additional sigmoid functions from sphereface

--- vectorstorm/sigmoid.h
+++ vectorstorm/sigmoid.h
@@ -4,6 +4,9 @@
 #include "vectorstorm/deprecated_macros.h"
 #include <cmath>
 #include "pi.h"
+#ifndef VERTORSTORM_NO_BOOST
+  #include <boost/math/special_functions/erf.hpp>
+#endif // VERTORSTORM_NO_BOOST
 
 #ifdef VECTORSTORM_NAMESPACE
 namespace VECTORSTORM_NAMESPACE {
@@ -43,9 +46,24 @@ inline static T constexpr sigmoid_log(T value) noexcept {
   /// Scale a 0-1 value to a logistic function - https://www.wolframalpha.com/input/?i=plot+1+%2F+%281+%2B+e^%28%280.5+-+x%29+*+8%29%29+from+0+to+1
   //return static_cast<T>(1) / (static_cast<T>(1) + std::exp((static_cast<T>(0.5) - value) * static_cast<T>(8)));
   // this ensures that we scale from exactly 0 to 1: https://www.wolframalpha.com/input/?i=plot+%28%28%281+%2F+%281+%2B+e^%28%280.5+-+x%29+*+8%29%29+-+0.5%29+*+%281+%2F+%281+%2F+%281+%2B+e^%28-4%29%29+-+0.5%29%29%29+*+0.5%29+%2B+0.5+from+x%3D0+to+1
-  T constexpr const scale = (static_cast<T>(1) / (static_cast<T>(1) / (static_cast<T>(1) + std::exp(static_cast<T>(-4))) - static_cast<T>(0.5)));
+  T constexpr const scale = static_cast<T>(1) / (static_cast<T>(1) / (static_cast<T>(1) + std::exp(static_cast<T>(-4))) - static_cast<T>(0.5));
   return ((((static_cast<T>(1) / (static_cast<T>(1) + std::exp((static_cast<T>(0.5) - value) * static_cast<T>(8)))) - static_cast<T>(0.5)) * scale) * static_cast<T>(0.5)) + static_cast<T>(0.5);
 };
+
+#ifndef VERTORSTORM_NO_BOOST
+
+template<typename T>
+inline static T constexpr sigmoid_erf(T value) noexcept __attribute__((__always_inline__));
+template<typename T>
+inline static T constexpr sigmoid_erf(T value) noexcept {
+  /// Scale a 0-1 value to an error function - https://www.wolframalpha.com/input/?i=plot+0.5+%2B+(0.5+*+erf((x+*+4)+-+2))+from+0+to+1
+  //return static_cast<T>(0.5) + (static_cast<T>(0.5) * boost::math::erf((value * static_cast<T>(4)) - static_cast<T>(2)));
+  // this ensures that we scale from exactly 0 to 1: https://www.wolframalpha.com/input/?i=plot+0.5+%2B+((0.5+%2F+erf(2.0))+*+erf((x+*+4.0)+-+2.0))+from+0+to+1
+  T const scale = static_cast<T>(0.5) / boost::math::erf(static_cast<T>(2));
+  return static_cast<T>(0.5) + (scale * boost::math::erf((value * static_cast<T>(4)) - static_cast<T>(2)));
+};
+
+#endif // VERTORSTORM_NO_BOOST
 
 #ifdef VECTORSTORM_NAMESPACE
 }
